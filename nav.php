6efadbbc902afde2bd417ef794c0232c18e47260
MaxTheMonster <maxungless@gmail.com>
2014-09-19 18:16:17 +0100

    Big commit
    Added styling, app support

---
+++ nav.php
@@ -0,0 +1,14 @@
+<html>
+<ank rel="stylesheet" type="text/css" href="styles/default.css">
+<div class="nav">
+<center>
+<ul>
+	<a href="#">Home</a>
+	<a href="#">Projects</a>
+	<a href="#">Add a Project</a>
+	<a href="#">Sign Up</a>
+</ul>
+</center>
+<input  placeholder="Search..." type="text" class="search"><button value="Search" class="button">Search</button>
+</div>
+</html>
